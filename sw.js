230d9dbcd4f5d97a106d91d5e72ca1a2529a623b
Michal Ortinský <ortinsky.michal@gmail.com>
2026-05-10 22:56:27 +0200

    Implement caching and fetch handling in sw.js
    Added caching for assets in service worker.

--- sw.js
+++ sw.js
@@ -1,6 +1,22 @@
+const CACHE_NAME = 'planovac-v1';
+const ASSETS = [
+    './',
+    './index.html',
+    './manifest.json'
+];
+
+// Instalace a uložení do paměti
 self.addEventListener('install', (e) => {
-    console.log('Service Worker nainstalován');
+    e.waitUntil(
+        caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+    );
 });
+
+// Zajištění chodu i bez internetu
 self.addEventListener('fetch', (e) => {
-    // Zatím jen propouští síťový provoz
+    e.respondWith(
+        caches.match(e.request).then((response) => {
+            return response || fetch(e.request);
+        })
+    );
 });
